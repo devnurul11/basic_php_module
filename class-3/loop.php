a604f9cae999baf56af76c33445ca16585cb0f85
Nurul Islam <devnurul11@gmail.com>
2023-02-07 01:40:04 +0600

    loop practice

---
+++ class-3/loop.php
@@ -0,0 +1,36 @@
+<?php
+
+// for loop
+
+for($i= 0; $i<=20; $i++){
+        echo PHP_EOL;
+        for ($j= 0; $j<$i ; $j++){
+        echo"*";
+
+    }
+}
+ 
+echo PHP_EOL;
+// while loop
+
+$i = 0;
+
+while ($i < 10){
+    $i++;
+    echo"I love you".PHP_EOL;
+}
+
+echo PHP_EOL;
+// do while loop
+$i = 0;
+do{
+    $i++;
+    echo "I love you".PHP_EOL;
+} while($i<9);
+
+// got to loop
+
+$i =0;
+a: $i++;
+echo"$i\n";
+if($i<10) goto a;
