359eaa2d50c0d15f19a63d8e895a566012cbffdc
Nurul Islam <devnurul11@gmail.com>
2023-02-09 01:46:22 +0600

    age calculator

--- class-3/function.php
+++ class-3/function.php
@@ -7,3 +7,16 @@ function addNumber($n1, $n2) {
 }
 
 echo addNumber(3, 58);
+
+echo PHP_EOL;
+
+function calculateAge($birthdate) {
+    $birthdate = new DateTime($birthdate);
+    $today = new DateTime();
+    $age = $today->diff($birthdate);
+    return $age->y ." Years ,". $age->m . "Months ,". $age->d. "days ." ;
+}
+
+$birthdate = "1994-11-25";
+$age = calculateAge($birthdate);
+echo "Age Is : ".$age;
