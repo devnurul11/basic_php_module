fa55aa17a643537700e6cdc59fbc20fcea70f5a2
Nurul Islam <devnurul11@gmail.com>
2023-02-09 01:16:33 +0600

    add functions

---
+++ class-3/function.php
@@ -0,0 +1,9 @@
+<?php
+
+function addNumber($n1, $n2) {
+    $result = $n1+$n2;
+    return $result;
+
+}
+
+echo addNumber(3, 58);
--- class-3/loop.php
+++ class-3/loop.php
@@ -34,3 +34,11 @@ $i =0;
 a: $i++;
 echo"$i\n";
 if($i<10) goto a;
+
+//foreach loop
+
+$cities = array("Dhaka, Khulna, Rajshahi, Barishal, Chottogram, Rangpur");
+
+foreach ( $cities as $key  => $city){
+    echo $city;
+}
